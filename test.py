0337710fc002860e96c61eddee660e5420988c00
amirekhlasi <35833203+amirekhlasi@users.noreply.github.com>
2019-02-15 19:54:59 +0330

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-w = tf.Variable([2.0, 3.0])
-opt = tf.train.AdamOptimizer()
-x = tf.reduce_sum(w*w)
-train = opt.minimize(x)
-print(tf.global_variables())
-print(tf.trainable_variables())
